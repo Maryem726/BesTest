b9fe56d0335f99132e542df6ce33ae45f96b8018
Eya Armaoui <eya.armaoui@esprit.tn>
2022-03-12 14:25:26 +0100

    user interface updated

--- frontend/UserFront/src/App.js
+++ frontend/UserFront/src/App.js
@@ -1,143 +1,152 @@
-import './App.css';
-//import home from './pages/home';
-import './styles/buttons.css';
-import './styles/footer.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import bannerimg from '../src/images/banner-img.png';
-import logoBestest from '../src/images/logoBestest.png';
-import parentPic from '../src/images/parent.png';
-import teacherPic from '../src/images/teacher.png';
-import kidPic from '../src/images/kid.png';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow
-} from 'mdb-react-ui-kit';
- 
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
 
-function App() {
+/*
+ * This is the entry point component of this project. You can change the below exported default App component to any of
+ * the prebuilt landing page components by uncommenting their import and export lines respectively.
+ * See one of the landing page components to better understand how to import and render different components (Always
+ * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
+ * disable the animation by using the disabled prop.
+ *
+ * The App component below is using React router to render the landing page that you see on the live demo website
+ * and the component previews.
+ *
+ */
+
+/* Use AnimationRevealPage as a wrapper component for your pages if you are building a custom one yourself */
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+/*
+ * Hero section is the top most section on the page. It contains the header as well.
+ * So you dont need to import headers
+ * separately
+ */
+
+// import Hero from "components/hero/TwoColumnWithVideo.js";
+// import Hero from "components/hero/TwoColumnWithInput.js";
+// import Hero from "components/hero/TwoColumnWithFeaturesAndTestimonial.js";
+// import Hero from "components/hero/TwoColumnWithPrimaryBackground.js";
+// import Hero from "components/hero/FullWidthWithImage.js";
+// import Hero from "components/hero/BackgroundAsImage.js";
+// import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
+
+// import Features from "components/features/ThreeColSimple.js";
+// import Features from "components/features/ThreeColWithSideImage.js";
+// import Features from "components/features/ThreeColWithSideImageWithPrimaryBackground.js";
+// import Features from "components/features/VerticalWithAlternateImageAndText.js";
+// import Features from "components/features/DashedBorderSixFeatures";
+// import MainFeature from "components/features/TwoColWithButton.js";
+// import MainFeature from "components/features/TwoColSingleFeatureWithStats.js";
+// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+// import MainFeature from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+// import FeatureWithSteps from "components/features/TwoColWithSteps.js";
+// import FeatureStats from "components/features/ThreeColCenteredStatsPrimaryBackground.js";
+
+// import Pricing from "components/pricing/ThreePlans.js";
+// import Pricing from "components/pricing/ThreePlansWithHalfPrimaryBackground.js";
+// import Pricing from "components/pricing/TwoPlansWithDurationSwitcher.js";
+
+// import SliderCard from "components/cards/ThreeColSlider.js";
+// import TrendingCard from "components/cards/TwoTrendingPreviewCardsWithImage.js";
+// import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
+// import TabGrid from "components/cards/TabCardGrid.js";
+
+// import Blog from "components/blogs/ThreeColSimpleWithImage.js";
+// import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
+// import Blog from "components/blogs/PopularAndRecentBlogPosts.js";
+// import Blog from "components/blogs/GridWithFeaturedPost.js";
+
+// import Testimonial from "components/testimonials/TwoColumnWithImage.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImageAndProfilePictureReview.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
+// import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+// import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
+
+// import FAQ from "components/faqs/SimpleWithSideImage.js";
+// import FAQ from "components/faqs/SingleCol.js";
+// import FAQ from "components/faqs/TwoColumnPrimaryBackground.js";
+
+// import ContactUsForm from "components/forms/SimpleContactUs.js";
+// import ContactUsForm from "components/forms/TwoColContactUsWithIllustration.js";
+// import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.js";
+//
+// import GetStarted from "components/cta/GetStarted.js";
+// import GetStarted from "components/cta/GetStartedLight.js";
+// import DownloadApp from "components/cta/DownloadApp.js";
+
+// import Footer from "components/footers/SimpleFiveColumn.js";
+// import Footer from "components/footers/FiveColumnWithInputForm.js";
+// import Footer from "components/footers/FiveColumnWithBackground.js";
+// import Footer from "components/footers/FiveColumnDark.js";
+// import Footer from "components/footers/MiniCenteredFooter.js";
+
+/* Ready Made Pages (from demos folder) */
+// import EventLandingPage from "demos/EventLandingPage.js";
+// import HotelTravelLandingPage from "demos/HotelTravelLandingPage.js";
+// import AgencyLandingPage from "demos/AgencyLandingPage.js";
+// import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
+// import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
+// import ServiceLandingPage from "demos/ServiceLandingPage.js";
+// import HostingCloudLandingPage from "demos/HostingCloudLandingPage.js";
+
+/* Inner Pages */
+// import LoginPage from "pages/Login.js";
+// import SignupPage from "pages/Signup.js";
+// import PricingPage from "pages/Pricing.js";
+// import AboutUsPage from "pages/AboutUs.js";
+// import ContactUsPage from "pages/ContactUs.js";
+// import BlogIndexPage from "pages/BlogIndex.js";
+// import TermsOfServicePage from "pages/TermsOfService.js";
+// import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+
+import ComponentRenderer from "ComponentRenderer.js";
+import MainLandingPage from "MainLandingPage.js";
+import ThankYouPage from "ThankYouPage.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  // If you want to disable the animation just use the disabled `prop` like below on your page's component
+  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
+
+
   return (
-
-
-   
-  
-   <div className="App" >
-     <h3 className="title">
-             WHATEVER THE PROBLEM BE PART OF
-            </h3>
-            
-            <div >
-              <img src ={logoBestest} />
-            <img src={bannerimg} alt="hero banner" />
-            </div>
-
-            <br/><br/><br/>
-            <center>
-            <h1 className="title">
-             Tell us who you are !
-            </h1><br/><br/>
-            <div className='bg-image' style={{ maxWidth: '28rem' }}>
-        
-        <img src={teacherPic} className='w-100' />
-        <div
-          className='mask'
-          style={{
-            background: 'linear-gradient(45deg, rgba(29, 236, 197, 0.7), rgba(91, 14, 214, 0.7) 100%)',
-          }}
-        >
-          <div className='d-flex justify-content-center align-items-center h-100'>
-                    <a class="btn btn-lg" href="#">
-                        <span>Teacher</span>
-                    </a>
-                </div>   
-          </div>
-      </div>
-                <br/><br/>
-
-
-                      <div className='bg-image' style={{ maxWidth: '28rem' }}>
-        
-        <img src={kidPic} className='w-100' />
-        <div
-          className='mask'
-          style={{
-            background: 'linear-gradient(45deg, rgba(29, 236, 197, 0.7), rgba(91, 14, 214, 0.7) 100%)',   
-          }}
-        >
-          <div className='d-flex justify-content-center align-items-center h-100'>
-                    <a class="btn btn-lg" href="#">
-                        <span>Kid</span>
-                    </a>
-                </div>   
-          </div>
-      </div>
-
-
-                <br/><br/>
-                <div className='bg-image' style={{ maxWidth: '28rem' }}>
-        
-        <img src={parentPic} className='w-100' />
-        <div
-          className='mask'
-          style={{
-            background: 'linear-gradient(45deg, rgba(29, 236, 197, 0.7), rgba(91, 14, 214, 0.7) 100%)',
-          }}
-        >
-          <div className='d-flex justify-content-center align-items-center h-100'>
-                    <a class="btn btn-lg" href="#">
-                        <span>Parent</span>
-                    </a>
-                </div>   
-          </div>
-      </div>    
-                </center>  
-                <div>
-
-
-
-
-    </div>
-                <MDBFooter  className='text-center text-lg-left'>
-      <MDBContainer className='p-1'>
-        <MDBRow>
-          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'><strong>Our project</strong></h5>
-
-            <p>
-            Welcome to BesTest web application dedicated for kids in El Fekir school currently enrolling between the 3rd and 6th primary grade in which they will be able to consult the list of lessons, exams and exercises provided by their teachers and also get involved in activities that pushes them to invest more in the application by providing them with fun side activities and getting rewarded with score points along the way depending on their state of completion of activities that will allow them to unlock more themes and more exciting activities. 
-The teacher will also have the opportunity to organize online catch-up sessions by hosting real time video calls with their students.
-The parent will also have the ability to organize private catch-up sessions for his kids in our site.
-
-            </p>
-          </MDBCol>
-
-          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'><strong>About Us</strong></h5>
-
-            <p>
-              We are a group of future engineers currently enrolling in ESPRIT who are fond of web development and we are happy to introduce to you
-              this application that we thought would be very beneficial for future generations in their education and providing their parents 
-              with more interesting opportunities to follow their kids progress.
-              We hope you enjoy our application and thank you in advance for your support.
-              <br/>
-              Cool as Code.
-            </p>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-dark' href='#'>
-          Cool As Code Group
-        </a>
-      </div>
-    </MDBFooter>
-
-             </div>  
+    <Router>
+      <Switch>
+        <Route path="/components/:type/:subtype/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/components/:type/:name">
+          <ComponentRenderer />
+        </Route>
+        <Route path="/thank-you">
+          <ThankYouPage />
+        </Route>
+        <Route path="/">
+          <MainLandingPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+// export default EventLandingPage;
+// export default HotelTravelLandingPage;
+// export default AgencyLandingPage;
+// export default SaaSProductLandingPage;
+// export default RestaurantLandingPage;
+// export default ServiceLandingPage;
+// export default HostingCloudLandingPage;
+
+// export default LoginPage;
+// export default SignupPage;
+// export default PricingPage;
+// export default AboutUsPage;
+// export default ContactUsPage;
+// export default BlogIndexPage;
+// export default TermsOfServicePage;
+// export default PrivacyPolicyPage;
+
+// export default MainLandingPage;
---
+++ frontend/UserFront/src/demos/RestaurantLandingPage.js
@@ -0,0 +1,120 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import Features from "components/features/ThreeColSimple.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+
+import chefIconImageSrc from "images/chef-icon.svg";
+import celebrationIconImageSrc from "images/celebration-icon.svg";
+import shopIconImageSrc from "images/shop-icon.svg";
+
+export default () => {
+  const Subheading = tw.span`tracking-wider text-sm font-medium`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl`;
+  return (
+    <AnimationRevealPage>
+      <Hero
+        heading={<> <HighlightedText>Ressources</HighlightedText></>}
+        description="welcome to our main ressources page in which you will find the list of lessons, exercices and exams available categorized by level and the teacher who uploaded them"
+        //imageSrc="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+        //imageCss={imageCss}
+        //imageDecoratorBlob={true}
+        
+      />
+      
+      {/* TabGrid Component also accepts a tabs prop to customize the tabs and its content directly. Please open the TabGrid component file to see the structure of the tabs props.*/}
+      <TabGrid
+        heading={
+          <>
+            Checkout our <HighlightedText>Lessons</HighlightedText>
+          </>
+        }
+      />
+      
+      
+     <MainFeature
+        subheading={<Subheading>Hello</Subheading>}
+        heading={
+          <>
+            We'll be presenting to you
+            <wbr /> <HighlightedText>our exercices</HighlightedText>
+          </>
+        }
+        
+        description={
+          <Description>
+           Time goes by fast when you're avoiding Homework...
+            <br />
+            <br />
+            
+          </Description>
+        }
+        
+        buttonRounded={false}
+        textOnLeft={false}
+        primaryButtonText="show"
+        imageSrc={
+          "https://img.myloview.fr/images/cartoon-little-boy-studying-on-the-table-400-187835570.jpg"
+        }
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
+      />
+
+
+      <MainFeature2
+        subheading={<Subheading>Hello</Subheading>}
+        heading={<>We'll be presenting to you <HighlightedText>our exames</HighlightedText></>}
+        description={
+          <Description>
+           Don't Stress...Do your Best!
+            <br />
+            <br />
+            
+          </Description>
+        }
+        statistics={[
+          {
+            key: "exames",
+            value: "100+",
+          },
+          {
+            key: "corrections",
+            value: "100+"
+          },
+          {
+            key: "teachers",
+            value: "many"
+          }
+        ]}
+        primaryButtonText="show"
+        primaryButtonUrl="https://order.now.com"
+        imageInsideDiv={false}
+        imageSrc="https://cdn1.vectorstock.com/i/thumb-large/13/80/happy-cute-little-student-boy-raising-his-hand-vector-38521380.jpg"
+        imageCss={Object.assign(tw`bg-cover`, imageCss)}
+        imageContainerCss={tw`md:w-1/2 h-auto`}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 md:w-32 md:h-32 -translate-x-1/2 opacity-25`}
+        textOnLeft={true}
+      />
+
+
+      <Testimonial
+        subheading=""
+        heading={<>Teachers <HighlightedText>About Us</HighlightedText></>}
+      />
+      
+      <Footer />
+    </AnimationRevealPage>
+  );
+}
